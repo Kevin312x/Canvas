e84aa74f2f5ff52fa80d2ec51981f94bbcd61a77
kevin312x <anmevee@yahoo.com>
2021-01-12 22:10:27 -0800

    added: drawing feature

--- canvas.js
+++ canvas.js
@@ -1,4 +1,29 @@
-window.addEventListener('load', () => {
+window.addEventListener('load', (e) => {
+    let draw_flag = false;
     const canvas = document.querySelector('#canvas');
     const ctx = canvas.getContext('2d');
+    canvas.height = 500;
+    canvas.width = 1500;
+    const mousedown_event = (e) => {
+        draw_flag = true;
+        ctx.beginPath();
+    }
+
+    const draw = (e) => {
+        if(!draw_flag) { return; }
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 5;
+        ctx.lineCap = 'round';
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+    }
+
+    const mouseup_event = () => {
+        draw_flag = false;
+    }
+
+
+    canvas.addEventListener('mousedown', mousedown_event);
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('mouseup', mouseup_event);
 });
